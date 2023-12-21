125a86d24ca51507b1e2696d68a7ceb37fa6a235
Ahinsak Raj <117904029+ahinsakraj@users.noreply.github.com>
2023-12-21 17:04:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function validEmail(str) {
   //your JS code here.
+	str=str.trim();
+	if(str.length==0)return false;
+	if(str[0]=='@')return false;
+	if(!str.includes('@'))return false;
+	if(!str.includes('.com') && !str.includes('.co.in') && !str.includes('.edu'))return false;
+	return true;
 }
 
 // Do not change the code below.
